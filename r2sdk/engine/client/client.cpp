fd5e2659ddae6ffe47e43d1048a2c28b93ac3214
EladNLG <e1lad8955@gmail.com>
2024-09-01 02:48:57 +0300

    Fix game crashing without placeholder pdata in SP

--- r2sdk/engine/client/client.cpp
+++ r2sdk/engine/client/client.cpp
@@ -4,29 +4,6 @@ bool CClient__Connect(void* self, const char* szName, void* pNetChannel, bool bF
 {
 	DevMsg(eDLL_T::NONE, "CClient__Connect\n");
 	bool bRet = v_CClient__Connect(self, szName, pNetChannel, bFakePlayer, a5, szMessage, a7);
-	if (!bRet)
-		return bRet;
-
-	// TODO [Fifty]: Use source fs for this
-	// TODO [Fifty]: Make this better
-	std::fstream pdataStream("./platform/placeholder_playerdata.pdata", std::ios_base::in);
-	if (!pdataStream.fail())
-	{
-		// get file length
-		pdataStream.seekg(0, pdataStream.end);
-		auto length = pdataStream.tellg();
-		pdataStream.seekg(0, pdataStream.beg);
-
-		// copy pdata into buffer
-		pdataStream.read((char*)self + 0x4FA, length);
-	}
-	else
-	{
-		Error(eDLL_T::ENGINE, EXIT_FAILURE, "Failed to read pdata!");
-	}
-
-	// Set persistence data as ready
-	*((char*)self + 0x4a0) = (char)0x3;
 
 	return bRet;
 }
