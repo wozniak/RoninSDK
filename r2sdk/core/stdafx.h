33e6abc21e4221fd46706d617d7de657bd4c9918
dyedquartz <dyedquartz@gmail.com>
2024-09-01 22:10:55 -0500

    moved most third party libraries to cmake fetch

--- r2sdk/core/stdafx.h
+++ r2sdk/core/stdafx.h
@@ -51,7 +51,7 @@
 #include <d3d11.h>
 #endif // !DEDICATED && !SDKLAUNCHER && !NETCONSOLE && !PLUGINSDK
 
-#include "thirdparty/nlohmann/json.hpp"
+#include <nlohmann/json.hpp>
 
 #include "thirdparty/detours/detours.h"
 #include "thirdparty/detours/idetour.h"
@@ -63,16 +63,16 @@
 //#include "thirdparty/lzham/include/lzham.h"
 #endif // !SDKLAUNCHER && !NETCONSOLE && !PLUGINSDK
 
-#include "thirdparty/spdlog/include/spdlog/spdlog.h"
-#include "thirdparty/spdlog/include/spdlog/async.h"
-#include "thirdparty/spdlog/include/spdlog/sinks/ostream_sink.h"
-#include "thirdparty/spdlog/include/spdlog/sinks/basic_file_sink.h"
-#include "thirdparty/spdlog/include/spdlog/sinks/stdout_sinks.h"
-#include "thirdparty/spdlog/include/spdlog/sinks/stdout_color_sinks.h"
-#include "thirdparty/spdlog/include/spdlog/sinks/ansicolor_sink.h"
-#include "thirdparty/spdlog/include/spdlog/sinks/rotating_file_sink.h"
+#include <spdlog/spdlog.h>
+#include <spdlog/async.h>
+#include <spdlog/sinks/ostream_sink.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_sinks.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/ansicolor_sink.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 
-#include "thirdparty/curl/include/curl/curl.h"
+#include <curl/curl.h>
 
 #include "common/experimental.h"
 #include "common/pseudodefs.h"
